e768e6be96d856fb9c77a060652f9ff0cfc0a163
Arshak Asriyan <aasriyan01@gmail.com>
2025-06-28 13:13:55 -0500

    Add Inngest integration with new API routes and client; update dependencies and UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
 export default function Home() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions());
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="p-4 max-w-7xl mx-auto">
       <h1>Hello World</h1>
+      <Button onClick={() => invoke.mutate({ text: "test@gmail.com" })}>
+        Invoke
+      </Button>
     </div>
   );
 }
